54c0e8b75b9e405f25f778cae694ed47e1578925
simyeongryu <rsm0503@gmail.com>
2020-02-27 13:11:03 +0900

    문제 25까지

---
+++ 021-030/note.js
@@ -0,0 +1,22 @@
+// 문제 21 set ???
+
+
+// 문제 22 변수 i가 6의 배수인지 확인하는 방법
+
+// 2) i % 6 == 0
+
+// 문제 23 OX
+
+console.log(10 / 3); // X
+
+// 문제 24 : 대문자로 바꾸기
+var name = "mary";
+
+console.log(name.toUpperCase());
+
+// 문제 25 : 원의 넓이 구하기
+function circle(n) {
+    return n * n * 3.14;
+}
+
+console.log(circle(3));
